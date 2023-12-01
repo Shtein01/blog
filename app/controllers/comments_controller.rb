b91f19743df3873abd9e921c638d08d9119a7246
Shtein01 <andrey96017@yandex.ru>
2023-12-01 13:56:11 +0500

    Доделал удаление статьи и комментариев
    rails importmap:install
    turbo:install stimulus:install

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,14 +8,24 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article), status: :see_other
+    article = Article.find(params[:article_id])
+    comment = article.comments.find(params[:id])
+    comment.destroy
+    # @article.find_by(comment_params1).destroy
+    redirect_to article_path(article), status: :see_other
   end
+
+  # def show
+  #   @article = Article.find(params[:article_id])
+  #   redirect_to article_path(@article)
+  # end
 
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body, :status)
+  end
+
+  def comment_params1
+    params.permit(:id, :article_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root "articles#index", as: 'home'
+  root 'articles#index'
 
   resources :articles do
     resources :comments
