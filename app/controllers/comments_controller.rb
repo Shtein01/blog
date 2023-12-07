3b81e9bee18d2b76cd4260547c023b0f3b908b54
Shtein01 <andrey96017@yandex.ru>
2023-12-07 12:30:18 +0500

    Редактирование комментария

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,21 +11,32 @@ class CommentsController < ApplicationController
     article = Article.find(params[:article_id])
     comment = article.comments.find(params[:id])
     comment.destroy
-    # @article.find_by(comment_params1).destroy
     redirect_to article_path(article), status: :see_other
   end
 
-  # def show
-  #   @article = Article.find(params[:article_id])
-  #   redirect_to article_path(@article)
-  # end
+  def show
+    @article = Article.find(params[:article_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:article_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body, :status)
-  end
-
-  def comment_params1
-    params.permit(:id, :article_id)
   end
 end
